555710213e92cd2acf2604e3199a55703d077e06
Arash1381-y <arashyadegari0402@gmail.com>
2024-01-30 16:10:13 +0330

    bug fix

--- backend/controllers/locationController.js
+++ backend/controllers/locationController.js
@@ -4,7 +4,7 @@ const {getLatestLocation} = require('../services/csv_reader')
 async function getCurrentLocation(req, res) {
     try {
         const result = await getLatestLocation()
-        res.status(200).json({time: result[2], lat: result[1], lon: result[2]})
+        res.status(200).json({time: result[0], lat: result[1], lon: result[2]})
     } catch (err) {
         res.status(500).send({message: err.message})
     }
