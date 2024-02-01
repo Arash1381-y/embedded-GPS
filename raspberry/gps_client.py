3488a3788e4fda18f65407f3bc55692b47063e65
halfblood <arashyadegari0402@gmail.com>
2024-02-01 18:25:04 +0330

    clean-up

--- raspberry/gps_client.py
+++ raspberry/gps_client.py
@@ -1,35 +1,65 @@
 import socket
-import serial
-import schedule
 import time
+
+import schedule
+import serial
+from dotenv import dotenv_values
 from pynmeagps import NMEAReader, NMEAMessage
+
 from utils import setup_custom_logger, get_timestamp
 
-SERIAL = '/dev/ttyAMA0'
-PERIPHERAL_BAUD_RATE = 9600
-UPDATE_INTERVAL = 20
+public_config = dotenv_values("./../.env.raspberry")
+secret_config = dotenv_values("./../.env.raspberry.private")
+
+CLIENT_SECRET_KEY = secret_config["gps_client_key"]
+
+SERIAL = public_config["serial_port"]
+PERIPHERAL_BAUD_RATE = (int(public_config["gps_module_baud_rate"]))
+
 DIST_THRESHOLD = 1e-4
-SERVER_ADDRESS = ('193.163.200.14', 9999)
+SERVER_ADDRESS = (public_config["server_ip"], int(public_config["server_port"]))
+
+# current location
 CL = (None, None)
 
-stream = serial.Serial(SERIAL, baudrate=9600)
+# create a NMEA reader with checksum validator
+stream = serial.Serial(SERIAL, baudrate=PERIPHERAL_BAUD_RATE)
 nmr = NMEAReader(stream, validate=0x01)
 
 logger = setup_custom_logger('gps')
 d_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-def post_data(lat, lon, timestamp):
+def send_data(lat: float, lon: float, timestamp: str):
+    """
+    send latitude and longitude
+
+    :param lat: latitude
+    :param lon: longitude
+    :param timestamp: global timestamp
+    :return: None
+    """
     logger.info("POST UPDATE")
-    msg = f"UPDATE {timestamp} {lat} {lon}"
+    msg = f"UPDATE {CLIENT_SECRET_KEY} {timestamp} {lat} {lon}"
     d_socket.sendto(msg.encode(), SERVER_ADDRESS)
 
 
 def is_any_loc():
+    """check if CL is initialized"""
     return CL[0] is not None and CL[1] is not None
 
 
-def get_dist(lat, lon):
+def get_dist(lat: float, lon: float):
+    """
+    compute dist of given point with current address
+
+    :param lat: latitude
+    :param lon: longitude
+    :return: CL dist from given point
+    """
+
+    assert type(CL[0]) is float and type(CL[1]) is float
+
     if is_any_loc():
         return ((lat - CL[0]) ** 2 + (lon - CL[1]) ** 2) ** 1 / 2
 
@@ -37,24 +67,37 @@ def get_dist(lat, lon):
 
 
 def get_location():
+    """
+    get location by parsing serial input
+
+    :return:
+    """
     global CL
-    for (raw_data, parsed_data) in nmr:
-        msg: NMEAMessage = parsed_data
-        pdt = msg.msgID
-        if pdt == 'RMC':
-            lat = None
-            lon = None
-            if msg.lat != "":
-                lat = float(msg.lat)
-            if msg.lon != "":
-                lon = float(msg.lon)
-            if msg.lon and msg.lat:
-                logger.info("latitude : " + str(lat) + " longitude : " + str(lon))
-                return lat, lon, get_timestamp()
+    try:
+        for (raw_data, parsed_data) in nmr:
+            msg: NMEAMessage = parsed_data
+            pdt = msg.msgID
+            if pdt == 'RMC':
+                lat = None
+                lon = None
+                if msg.lat != "":
+                    lat = float(msg.lat)
+                if msg.lon != "":
+                    lon = float(msg.lon)
+                if msg.lon and msg.lat:
+                    logger.info("latitude : " + str(lat) + " longitude : " + str(lon))
+                    return lat, lon, get_timestamp()
+    except AttributeError:
+        pass
 
 
-def update_location(force: bool = False):
-    if not force:
+def update_location(force_update: bool = False):
+    """
+    get current location and send it to the server
+
+    :param force_update: if true no need to check the dist threshold
+    """
+    if not force_update:
         logger.info("Check location...")
     else:
         logger.info("Update location...")
@@ -65,15 +108,16 @@ def update_location(force: bool = False):
         return
 
     d = get_dist(lat, lon)
-    if d > DIST_THRESHOLD or d < 0:
-        post_data(lat, lon, timestamp)
-    elif force:
-        post_data(lat, lon, timestamp)
+    if d > DIST_THRESHOLD or d < 0 or force_update:
+        send_data(lat, lon, timestamp)
 
+    # update location
     CL = (lat, lon)
 
 
+# check every 15 sec for new location
 schedule.every(15).seconds.do(update_location)
+# send an update every minute
 schedule.every().minutes.do(update_location, force=True)
 
 
--- raspberry/gps_server.py
+++ raspberry/gps_server.py
@@ -2,29 +2,61 @@ import csv
 import socket
 
 from utils import setup_custom_logger
+from dotenv import dotenv_values
 
-SERVER_ADDRESS = ('', 9999)
-logger = setup_custom_logger('server')
+public_config = dotenv_values("./../.env.raspberry")
+secret_config = dotenv_values("./../.env.raspberry.private")
+
+CLIENT_SECRET_KEY = secret_config["gps_client_key"]
+
+SERVER_ADDRESS = (public_config["server_ip"], int(public_config["server_port"]))
+LOG_FILE = public_config["data_csv"]
+logger = setup_custom_logger("server")
+
+BUFFER_SIZE = 4096
+
+
+def check_key(key):
+    """
+    check if given message is from expected client or not
+    :param key: input key
+    :return: true if client send key. o.w false
+    """
+    return key == CLIENT_SECRET_KEY
 
 
 def update_csv(data):
+    """
+    update csv with receive data
+
+    :param data: input data
+    :return:
+    """
+
+    # check if update message is sent
     if data.startswith("UPDATE"):
-        lat, lon, timestamp = data.split(" ")[1:]
-        with open('../gps.csv', 'a', newline='') as csvfile:
-            wr = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            wr.writerow([lat, lon, timestamp])
+        _, key, lat, lon, timestamp = data.split(" ")
+        # check if msg contains correct key
+        if check_key(key):
+            with open(LOG_FILE, 'a', newline='') as csvfile:
+                wr = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                wr.writerow([lat, lon, timestamp])
 
 
 def run():
+    """
+    run server on given address and wait for gps client data
+    :return:
+    """
+    # create a tcp socket and wait for connection
     s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s_socket.bind(SERVER_ADDRESS)
 
     while True:
-        data, adr = s_socket.recvfrom(4096)
+        data, adr = s_socket.recvfrom(BUFFER_SIZE)
         logger.info("RECEIVE: " + data.decode())
         update_csv(data.decode())
 
 
-#
 run()
